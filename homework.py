c9f11a6192eb23af0c57d6505d108acac4d3984c
Мария Константинова <wildcat.3333@yandex.ru>
2023-03-05 16:07:58 +0700

    PEP8 edit

--- homework.py
+++ homework.py
@@ -4,7 +4,6 @@ import os
 import requests
 import time
 import telegram
-from threading import Timer
 
 
 load_dotenv()
@@ -15,15 +14,10 @@ logging.basicConfig(
     format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
 )
 
-# # А тут установлены настройки логгера для текущего файла - example_for_log.py
-# logger = logging.getLogger(__name__)
-# # Устанавливаем уровень, с которого логи будут сохраняться в файл
-# logger.setLevel(logging.INFO)
-
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-TELEGRAM_CHAT_ID = '463435736'
+# TELEGRAM_CHAT_ID = '463435736'
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -45,7 +39,10 @@ def check_tokens():
     if PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID:
         return True
     else:
-        logging.critical('Проверьте переменные окружения! PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID')
+        logging.critical('Проверьте переменные окружения! PRACTICUM_TOKEN,'
+                         'TELEGRAM_TOKEN, TELEGRAM_CHAT_ID')
+        raise Exception('Проверьте переменные окружения! PRACTICUM_TOKEN,'
+                        'TELEGRAM_TOKEN, TELEGRAM_CHAT_ID')
 
 
 def send_message(bot, message):
@@ -72,9 +69,12 @@ def get_api_answer(timestamp):
         homework_statuses = requests.get(
             ENDPOINT, headers=HEADERS, params=timestamp
         )
-        
+
         if homework_statuses.status_code != 200:
-            raise Exception(f'Код ошибки при запросе к API: {homework_statuses.status_code}')
+            logging.error(f'Код ошибки при запросе к API: '
+                          f'{homework_statuses.status_code}')
+            raise Exception(f'Код ошибки при запросе к API: '
+                            f'{homework_statuses.status_code}')
         return homework_statuses.json()
     except requests.RequestException as error:
         logging.error(f'Ошибка при запросе к API: {error}')
@@ -85,18 +85,19 @@ def check_response(response):
     функция получает ответ API, приведенный к типам данных Python."""
     try:
         if 'homeworks' not in response:
-            logging.error(f'В ответе API домашки нет ключа `homeworks`.')
-            raise TypeError(f'В ответе API домашки нет ключа `homeworks`.')
+            logging.error('В ответе API домашки нет ключа `homeworks`.')
+            raise TypeError('В ответе API домашки нет ключа `homeworks`.')
 
         if type(response['homeworks']) is not list:
-            logging.error(f'В ответе API тип данных не соответствует ожиданиям.')
-            raise TypeError(f'В ответе API тип данных не соответствует ожиданиям.')
+            logging.error('В ответе API тип данных не'
+                          'соответствует ожиданиям.')
+            raise TypeError('В ответе API тип данных не'
+                            'соответствует ожиданиям.')
 
         if 'homeworks' in response:
             return True
 
     except KeyError as error:
-        # print(error)
         logging.error(f'Отсутствует ключ homeworks в ответе API: {error}')
 
 
@@ -108,64 +109,71 @@ def parse_status(homework):
     словаря HOMEWORK_VERDICTS."""
     homework_name = homework.get('homework_name')
     status = homework.get('status')
-    # old_status = status
 
     try:
         for status_key in HOMEWORK_VERDICTS.keys():
-            # old_status = status
             if not type(status_key) is str:
-                raise logging.error(f'Функция `parse_status` возвращает не строку: {type(status_key)}')
+                logging.error(f'Функция `parse_status` возвращает не строку: '
+                              f'{type(status_key)}')
+                raise Exception(f'Функция `parse_status` возвращает '
+                                f'не строку: {type(status_key)}')
 
             if not homework_name:
-                raise logging.error(f'В ответе функции `parse_status` не содержится название домашней работы: {homework_name}')
+                logging.error(f'В ответе функции `parse_status` не содержится'
+                              f' название домашней работы: {homework_name}')
+                raise Exception(f'В ответе функции `parse_status` '
+                                f'не содержится название домашней работы: '
+                                f'{homework_name}')
 
             if status not in HOMEWORK_VERDICTS.keys():
-                raise logging.error(f'В ответе функции `parse_status` не содержится название домашней работы: {homework_name}')
+                logging.error(f'В ответе функции `parse_status` не содержится '
+                              f'название домашней работы: {homework_name}')
+                raise Exception(f'В ответе функции `parse_status` не '
+                                f'содержится название домашней работы: '
+                                f'{homework_name}')
 
-            if (status_key == status):# and (old_status != status) and (old_status in HOMEWORK_VERDICTS.keys()):
+            if (status_key == status):
                 verdict = HOMEWORK_VERDICTS[status_key]
-                message = f'Изменился статус проверки работы "{homework_name}". {verdict}'
+                message = (f'Изменился статус проверки работы '
+                           f'"{homework_name}". '
+                           f'{verdict}')
                 return message
     except KeyError as error:
-        logging.error(f'Неожиданный статус домашней работы, в ответе API: {error}')
+        logging.error(f'Неожиданный статус домашней работы, в ответе API: '
+                      f'{error}')
 
 
 def main():
     """Основная логика работы бота."""
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    old_status = None
+    check_tokens()
+
     try:
-        if check_tokens():
-            # timestamp = int(time.time())
-            # PAYLOAD = {'from_date': timestamp}
-            # print(timestamp)
-            api_answer = get_api_answer(PAYLOAD)
-            # print(api_answer)
-            # print(type(api_answer))
-            # print(list(api_answer))
 
-            if check_response(list(api_answer)):
-                if api_answer.get('homeworks')[0]['homework_name'] == HOMEWORK:
-                    homework = api_answer.get('homeworks')[0]
+        timestamp = int(time.time())
+        PAYLOAD = {'from_date': timestamp}
+        print(timestamp)
+        api_answer = get_api_answer(PAYLOAD)
+        print(api_answer)
 
-                    print(homework)
+        if check_response(api_answer):
+            if api_answer.get('homeworks')[0]['homework_name'] == HOMEWORK:
+                homework = api_answer.get('homeworks')[0]
 
+                if old_status is None:
+                    old_status = homework['status']
+
+                if old_status != homework['status']:
                     message = parse_status(homework)
-                    # send_message(bot, message)
+                    send_message(bot, message)
 
-                time.sleep(RETRY_PERIOD)
+            time.sleep(RETRY_PERIOD)
     except Exception as error:
         message = f'Сбой в работе программы: {error}'
-        logging.critical(error)
-    # while True:
-    #     try:
-
-    #        ...
-
-    #     except Exception as error:
-    #         message = f'Сбой в работе программы: {error}'
-    #         ...
-    #     ...
+        logging.critical(f'Сбой в работе программы: {error}')
 
 
 if __name__ == '__main__':
-    main()
+    while True:
+        main()
